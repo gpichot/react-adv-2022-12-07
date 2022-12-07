f5f1262922dc1fbc4bcfc1e1be99321ca0620ac1
Gabriel Pichot <gabriel.pichot@gmail.com>
2022-12-07 12:25:23 +0100

    feat: initial commit

---
+++ pokeapp/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import classnames from "classnames";
+
+import reactLogo from "./assets/react.svg";
+
+import "./globals.scss";
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div>
+      <h1>Pokemons</h1>
+    </div>
+  );
+}
+
+export default App;
